61b83d173ec95d455852e652643014e065afd3d0
Vlad Holubiev <golubev.vld@gmail.com>
2017-03-30 14:39:57 -0400

    Ask for next page of services if present

--- src/ls.js
+++ src/ls.js
@@ -2,10 +2,19 @@ import {ECS} from 'aws-sdk';
 
 let ecs;
 
-export async function ls(region = 'us-east-1', cluster = '') {
+export async function ls(region = 'us-east-1', cluster = '', nextToken = '') {
   if (!ecs) {
     ecs = new ECS({region});
   }
 
-  return await ecs.listServices({cluster}).promise();
+  const {serviceArns, nextToken: recentNextToken} = await ecs.listServices({
+    cluster,
+    nextToken
+  }).promise();
+
+  if (recentNextToken) {
+    serviceArns.push(...await ls(region, cluster, recentNextToken));
+  }
+
+  return serviceArns;
 }
--- src/ls.test.js
+++ src/ls.test.js
@@ -2,13 +2,30 @@ import AWS from 'aws-sdk-mock';
 
 import {ls} from './ls';
 
+beforeEach(() => {
+  AWS.restore('ECS', 'listServices');
+});
+
 it('should export ls function', () => {
   expect(ls).toBeInstanceOf(Function);
 });
 
 it('should invoke listServices function', async() => {
-  AWS.mock('ECS', 'listServices', []);
+  AWS.mock('ECS', 'listServices', {serviceArns: ['1']});
   const services = await ls();
 
-  expect(services).toEqual([]);
+  expect(services).toEqual(['1']);
+});
+
+it('should invoke listServices again if nextToken is present', async() => {
+  AWS.mock('ECS', 'listServices', (params, cb) => {
+    if (params.nextToken) {
+      return cb(null, {serviceArns: ['2']});
+    }
+
+    return cb(null, {serviceArns: ['1'], nextToken: 'token'});
+  });
+  const services = await ls();
+
+  expect(services).toEqual(['1', '2']);
 });
