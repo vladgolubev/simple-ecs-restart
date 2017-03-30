92d24241b7068d449df61f9169d82a3e1f05c21d
Vlad Holubiev <golubev.vld@gmail.com>
2017-03-30 15:15:09 -0400

    Wait for a complete shutdown

--- src/stop.js
+++ src/stop.js
@@ -1,4 +1,5 @@
 import {ECS} from 'aws-sdk';
+import Promise from 'bluebird';
 
 let ecs;
 
@@ -7,7 +8,23 @@ export async function stop(region = 'us-east-1', cluster = '', service = 'ß') {
     ecs = new ECS({region});
   }
 
-  const {desiredCount, runningCount, pendingCount} = await ecs.updateService({cluster, service, desiredCount: 0}).promise();
+  await shutdown(cluster, service);
+  await waitForShutdown(cluster, service);
 
-  return runningCount;
+  return 0;
+}
+
+async function shutdown(cluster, service) {
+  await ecs.updateService({cluster, service, desiredCount: 0}).promise();
+}
+
+async function waitForShutdown(cluster, service) {
+  const servicesStatus = await ecs.describeServices({cluster, services: [service]}).promise();
+
+  const {runningCount, pendingCount} = servicesStatus.services[0];
+
+  if (runningCount !== 0 || pendingCount !== 0) {
+    await Promise.delay(1000);
+    await waitForShutdown(cluster, service);
+  }
 }
--- src/stop.test.js
+++ src/stop.test.js
@@ -4,6 +4,7 @@ import {stop} from './stop';
 
 beforeEach(() => {
   AWS.restore('ECS', 'updateService');
+  AWS.restore('ECS', 'describeServices');
 });
 
 it('should export stop function', () => {
@@ -11,8 +12,82 @@ it('should export stop function', () => {
 });
 
 it('should invoke updateService function', async() => {
-  AWS.mock('ECS', 'updateService', {desiredCount: 0, runningCount: 0, pendingCount: 0});
+  const updateServiceMock = jest.fn((params, cb) => {
+    return cb(null, {desiredCount: 0, runningCount: 0, pendingCount: 0});
+  });
+  const describeServicesMock = jest.fn((params, cb) => {
+    return cb(null, {
+      services: [{desiredCount: 0, runningCount: 0, pendingCount: 0}]
+    });
+  });
 
-  const response = await stop();
-  expect(response).toBe(0);
+  AWS.mock('ECS', 'updateService', updateServiceMock);
+  AWS.mock('ECS', 'describeServices', describeServicesMock);
+
+  await stop();
+
+  expect(updateServiceMock).toBeCalled();
+});
+
+it('should invoke describeServices if service has pending tasks', async() => {
+  const updateServiceMock = jest.fn((params, cb) => {
+    return cb(null, {desiredCount: 0, runningCount: 0, pendingCount: 1});
+  });
+  const describeServicesMock = jest.fn((params, cb) => {
+    return cb(null, {
+      services: [{desiredCount: 0, runningCount: 0, pendingCount: 0}]
+    });
+  });
+
+  AWS.mock('ECS', 'updateService', updateServiceMock);
+  AWS.mock('ECS', 'describeServices', describeServicesMock);
+
+  await stop();
+
+  expect(describeServicesMock).toBeCalled();
+});
+
+it('should invoke describeServices again if service has still pending tasks', async() => {
+  const updateServiceMock = jest.fn((params, cb) => {
+    return cb(null, {desiredCount: 0, runningCount: 0, pendingCount: 1});
+  });
+  const describeServicesMock = jest.fn()
+    .mockImplementationOnce((params, cb) => {
+      return cb(null, {
+        services: [{desiredCount: 0, runningCount: 0, pendingCount: 1}]
+      });
+    })
+    .mockImplementationOnce((params, cb) => {
+      return cb(null, {
+        services: [{desiredCount: 0, runningCount: 0, pendingCount: 1}]
+      });
+    })
+    .mockImplementationOnce((params, cb) => {
+      return cb(null, {
+        services: [{desiredCount: 0, runningCount: 0, pendingCount: 0}]
+      });
+    });
+
+  AWS.mock('ECS', 'updateService', updateServiceMock);
+  AWS.mock('ECS', 'describeServices', describeServicesMock);
+
+  await stop();
+
+  expect(describeServicesMock).toBeCalled();
+});
+
+it('should return 0', async() => {
+  const updateServiceMock = jest.fn((params, cb) => {
+    return cb(null, {desiredCount: 0, runningCount: 0, pendingCount: 1});
+  });
+  const describeServicesMock = jest.fn((params, cb) => {
+    return cb(null, {
+      services: [{desiredCount: 0, runningCount: 0, pendingCount: 0}]
+    });
+  });
+
+  AWS.mock('ECS', 'updateService', updateServiceMock);
+  AWS.mock('ECS', 'describeServices', describeServicesMock);
+
+  expect(await stop()).toBe(0);
 });
